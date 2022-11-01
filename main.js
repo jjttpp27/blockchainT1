50ff360d31a75efad40d42db6344a7fc75afd2a5
Juan <juanjtp92@gmail.com>
2022-11-01 21:50:23 +0100

    nuevas actualizaciones en un recuadro

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 
 var encodedString;
 
+//Primero creamos la funcion para encriptar o codificar y despues crearemos la segunda funcion para desencriptar o codificar.
+
 function encrypt() {
+
+    /* La variable baseString sera un array con los valores que ingrese el usuario por lo que usamos 
+     el getElementById para seleccionarlo y .value() para convertirel string en un array.*/
     var baseString = document.getElementById("enc").value;
+
+    // En la variable encodedString se guardara la codificacion del String ingresado por el usuario.
     encodedString = window.btoa(baseString);
+
+    //Finalmente decidimos donde se va mostrar el texto codificado
     document.getElementById("dec").value = encodedString;
 }
 
 function decrypt() {
+
+    //Guardamos en decodedString el valor decodificado de "encodedString"
     var decodedString = window.atob(encodedString);
-    document.getElementById("dec").value = decodedString;
+
+    //Decidimos donde mostrar el mensaje decodificado
+    document.getElementById("res").value = decodedString;
 }
 
 
